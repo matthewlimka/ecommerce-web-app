950da58a97ce6669f6e8513dc89db59d4db4876e
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-01-16 18:50:28 +0800

    Placeholder implementation for Spring Security

---
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/auth/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.matthewlim.ecommercewebapp.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(request -> {
+				request.requestMatchers("/api/v1/**").permitAll();
+				request.requestMatchers("/**").permitAll();
+			})
+			.httpBasic(Customizer.withDefaults());
+		
+		return http.build();
+	}
+}
