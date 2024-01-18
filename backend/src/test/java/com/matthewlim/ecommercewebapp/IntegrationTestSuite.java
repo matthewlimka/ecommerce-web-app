732821c78f617f31153d5b2242830b5be054d918
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-01-18 18:54:13 +0800

    Shifted to integration test suite class to suite package

--- backend/src/test/java/com/matthewlim/ecommercewebapp/IntegrationTestSuite.java
+++
@@ -1,8 +0,0 @@
-package com.matthewlim.ecommercewebapp;
-
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectPackages({"com.matthewlim.ecommerceapp.controllers.integration"})
-public class IntegrationTestSuite {}
