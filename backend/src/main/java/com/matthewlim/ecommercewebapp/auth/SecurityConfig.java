f2af37c52c0a7980d97d20c506dc01c6d5bc9215
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-01-18 18:52:50 +0800

    Updated SecurityConfig class

--- backend/src/main/java/com/matthewlim/ecommercewebapp/auth/SecurityConfig.java
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/auth/SecurityConfig.java
@@ -2,15 +2,30 @@ package com.matthewlim.ecommercewebapp.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+	    UserDetails user = User.withUsername("user")
+	      .password(passwordEncoder().encode("userPass"))
+	      .roles("USER")
+	      .build();
+	    return new InMemoryUserDetailsManager(user);
+	}
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -18,14 +33,11 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
-			.csrf(csrf -> csrf.disable())
-			.authorizeHttpRequests(request -> {
-				request.requestMatchers("/api/v1/**").permitAll();
-				request.requestMatchers("/**").permitAll();
-			})
-			.httpBasic(Customizer.withDefaults());
-		
-		return http.build();
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
+                .authenticated())
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
 	}
 }
