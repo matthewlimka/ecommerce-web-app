5c4f49ae3faf3df9c06694d91f41024d3b1da5e8
Matthew Lim <matthewlimka@gmail.com>
2024-01-23 16:54:37 +0800

    Added configuration properties for RSA Key

--- backend/src/main/java/com/matthewlim/ecommercewebapp/ECommerceWebAppApplication.java
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/ECommerceWebAppApplication.java
@@ -2,7 +2,11 @@ package com.matthewlim.ecommercewebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.matthewlim.ecommercewebapp.auth.RsaKeyProperties;
+
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class ECommerceWebAppApplication {
 
