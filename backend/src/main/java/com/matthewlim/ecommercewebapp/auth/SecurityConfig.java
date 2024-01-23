562c764fa08ef22d50253fecf6b6ff961a7313ab
Matthew Lim <matthewlimka@gmail.com>
2024-01-23 17:27:13 +0800

    Added corsConfigurer, JWT encoder and decoder beans, updated security filter chain

--- backend/src/main/java/com/matthewlim/ecommercewebapp/auth/SecurityConfig.java
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/auth/SecurityConfig.java
@@ -1,43 +1,100 @@
 package com.matthewlim.ecommercewebapp.auth;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.matthewlim.ecommercewebapp.services.UserService;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	private final UserService userService;
+	private final RsaKeyProperties rsaKeyProperties;
+	
 	@Bean
 	public InMemoryUserDetailsManager userDetailsService() {
-	    UserDetails user = User.withUsername("user")
-	      .password(passwordEncoder().encode("userPass"))
-	      .roles("USER")
-	      .build();
-	    return new InMemoryUserDetailsManager(user);
+		return new InMemoryUserDetailsManager(User
+				.withUsername("user")
+				.password(passwordEncoder().encode("password"))
+				.authorities("read")
+				.build());
 	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder
+				.withPublicKey(rsaKeyProperties.publicKey())
+				.build();
+	}
+	
+	@Bean
+	public JwtEncoder jwtEncoder() {
+		JWK jwk = new RSAKey
+				.Builder(rsaKeyProperties.publicKey())
+				.privateKey(rsaKeyProperties.privateKey())
+				.build();
+		JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+		return new NimbusJwtEncoder(jwks);
+	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
-                .authenticated())
-                .httpBasic(withDefaults())
-                .formLogin(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable);
-        return http.build();
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+        		.csrf(csrf -> csrf.disable())
+//        		.securityMatcher("/api/**")
+        		.authorizeHttpRequests(authorize -> authorize
+        			.requestMatchers("/", "/error", "/api/v1/login", "/api/v1/login/oauth2", "/api/v1/token").permitAll()
+        			.requestMatchers("/api/v1/**").authenticated()
+        			.anyRequest().authenticated()
+        		)
+        		.oauth2Login(withDefaults())
+//        		.formLogin(withDefaults())
+        		.oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+        		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowCredentials(true);
+            }
+        };
+    }
 }
