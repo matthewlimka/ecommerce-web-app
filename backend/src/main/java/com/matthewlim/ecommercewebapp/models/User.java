58945f62674eeae55871d66b8c4cc49b3d4a4edd
Matthew Lim <matthewlimka@gmail.com>
2024-02-02 16:23:13 +0800

    Re-added Role field to User class and updated constructor accordingly

--- backend/src/main/java/com/matthewlim/ecommercewebapp/models/User.java
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/models/User.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.matthewlim.ecommercewebapp.enums.Role;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,19 +34,22 @@ public class User {
 
 	@Column(unique = true)
 	private String username;
-	
+
 	private String password;
-	
+
 	@Column(unique = true)
 	private String email;
-	
+
 	private String firstName;
 	private String lastName;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
 	@JsonIgnore
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	private List<Order> orders;
-	
+
 	@JsonIgnore
 	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
 	private Address address;
@@ -51,15 +57,16 @@ public class User {
 	@JsonIgnore
 	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
 	private Cart cart;
-	
-	public User(String username, String password, String email, String firstName, String lastName,
-			List<Order> orders, Address address, Cart cart) {
+
+	public User(String username, String password, String email, String firstName, String lastName, Role role, List<Order> orders,
+			Address address, Cart cart) {
 		super();
 		this.username = username;
 		this.password = password;
 		this.email = email;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.role = role;
 		this.orders = orders;
 		this.address = address;
 		this.cart = cart;
