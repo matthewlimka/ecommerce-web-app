00f4a72fbd199e21ecf8190641bbca7b16701e96
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-02-20 22:44:32 +0800

    Added APIProvider and route for ResultPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { APIProvider } from './contexts/APIContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import './App.css';
-import { AuthProvider } from './contexts/AuthContext';
 import OAuthCallbackPage from './pages/OAuthCallbackPage';
+import ResultPage from './pages/ResultPage';
 
 function App() {
   return (
     <AuthProvider>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/oauth/callback" element={<OAuthCallbackPage />} />
-        </Routes>
-      </div>
+      <APIProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/oauth/callback" element={<OAuthCallbackPage />} />
+            <Route path="/result" element={<ResultPage />} />
+          </Routes>
+        </div>
+      </APIProvider>
     </AuthProvider>
   );
 }
