de83a6eecb6007fb8f73d23b79812f7cf63852a4
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-02-20 22:46:23 +0800

    Added Navbar, API call to fetch user with JWT and changed HomePage message

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,23 +1,29 @@
-import { useAuth } from '../contexts/AuthContext';
+import Navbar from '../components/Navbar';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import { useAPI } from '../contexts/APIContext';
 
 const HomePage: React.FC = () => {
 
-    const { jwt, login } = useAuth();
+    const { jwt } = useAuth();
+    const { user, getUser } = useAPI();
     const navigate = useNavigate();
 
     useEffect(() => {
         if (jwt === null) {
             console.log('Not logged in, redirecting to login page')
             navigate('/login')
+        } else {
+            getUser(jwt)
         }
-    }, [jwt])
+    }, [])
 
     return (
         <div>
-            <h1>Welcome to the home page!</h1>
-            <p>Your JWT is {jwt}</p>
+            <Navbar />
+            <h1>Home Page</h1>
+            <p>Welcome to ShoppersGate {user}!</p>
         </div>
     )
 }
