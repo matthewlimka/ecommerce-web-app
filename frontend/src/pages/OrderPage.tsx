9fc3d7869812b5275addf9b096a72c0c00a6ec06
Matthew Lim <matthewlimka@gmail.com>
2024-02-27 13:27:08 +0800

    Added OrderPage

---
+++ frontend/src/pages/OrderPage.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+const OrderPage: React.FC = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Order Page</h1>
+        </div>
+    )
+}
+
+export default OrderPage;
