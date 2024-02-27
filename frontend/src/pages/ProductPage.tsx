c6f4b0dd90c45315b2fce1a9c2990655e26e8c14
Matthew Lim <matthewlimka@gmail.com>
2024-02-27 13:26:38 +0800

    Added ProductPage

---
+++ frontend/src/pages/ProductPage.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+const ProductPage: React.FC = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Product Page</h1>
+        </div>
+    )
+}
+
+export default ProductPage;
