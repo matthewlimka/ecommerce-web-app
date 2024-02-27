e93b0ec94f4dd7b8f1ee713c402c1ca9c164d000
Matthew Lim <matthewlimka@gmail.com>
2024-02-27 13:46:01 +0800

    Added JwtAuthorizationFilter bean and added it to security filter chain

--- backend/src/main/java/com/matthewlim/ecommercewebapp/auth/SecurityConfig.java
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/auth/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -85,6 +86,7 @@ public class SecurityConfig {
         		.exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint()))
         		.oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
         		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        		.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .build();
 	}
 	
@@ -115,6 +117,11 @@ public class SecurityConfig {
 	@Bean
 	public JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint() {
 		return new JwtAuthenticationEntryPoint();
+	}
+	
+	@Bean
+	public JwtAuthorizationFilter jwtAuthorizationFilter() {
+	    return new JwtAuthorizationFilter(jwtDecoder());
 	}
 	
 	@Bean
