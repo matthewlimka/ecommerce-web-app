7b10c3826838ba58661e772b89270ccdbbab3c0c
Matthew Lim <matthewlimka@gmail.com>
2024-02-27 13:26:07 +0800

    Added CartPage

---
+++ frontend/src/pages/CartPage.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+const CartPage: React.FC = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Cart Page</h1>
+        </div>
+    )
+}
+
+export default CartPage;
