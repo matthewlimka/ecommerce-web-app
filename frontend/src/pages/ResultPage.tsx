bbb95703df60626d81da67cb48c9e6d5d83af713
Matthew Lim <matthewlimka@gmail.com>
2024-02-27 13:35:38 +0800

    Added useEffect hook to fetch user if logged in and modified page HTML slightly

--- frontend/src/pages/ResultPage.tsx
+++ frontend/src/pages/ResultPage.tsx
@@ -7,20 +7,29 @@ import Navbar from '../components/Navbar';
 const ResultPage: React.FC = () => {
 
     const { jwt } = useAuth();
-    const { queryResults, getQueryResults } = useAPI();
+    const { getUser, queryResults, getQueryResults } = useAPI();
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const query = queryParams.get('q');
 
     useEffect(() => {
-        getQueryResults(query);
-    }, [])
+        if (jwt !== null) {
+            getUser(jwt)
+        }
+    }, [jwt])
+
+    useEffect(() => {
+        if (query !== null) {
+            getQueryResults(query);
+        } else {
+            console.log('No query received');
+        }
+    }, [query])
 
     return (
         <div>
             <Navbar />
-            <h1>Result Page</h1>
-            <p>You searched for: {query}</p>
+            <h1>X results for {query}</h1>
             <ul>
                 {queryResults.map((result, index) => (
                     <li key={index}>{result}</li>
