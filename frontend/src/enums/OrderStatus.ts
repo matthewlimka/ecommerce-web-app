0cbe9bd71b14b51f45539520243c8c0aa1f740bc
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-02-29 18:02:08 +0800

    Created enums

---
+++ frontend/src/enums/OrderStatus.ts
@@ -0,0 +1,9 @@
+enum OrderStatus {
+    PENDING = 'PENDING',
+    PROCESSING = 'PROCESSING',
+    SHIPPED = 'SHIPPED',
+    DELIVERED = 'DELIVERED',
+    CANCELLED = 'CANCELLED'
+}
+
+export default OrderStatus;
---
+++ frontend/src/enums/PaymentMethod.ts
@@ -0,0 +1,7 @@
+enum PaymentMethod {
+    CREDIT_CARD_MASTERCARD = 'CREDIT_CARD_MASTERCARD',
+    CREDIT_CARD_VISA = 'CREDIT_CARD_VISA',
+    PAYNOW = 'PAYNOW',
+}
+
+export default PaymentMethod;
