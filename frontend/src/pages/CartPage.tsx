8592d23eb6c34791543722bbf949e064d47d0016
Matthew Lim <matthewlimka@gmail.com>
2024-03-05 17:11:36 +0800

    Updated CartPage to show user's cart

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,12 +1,48 @@
-import Navbar from '../components/Navbar';
+import "../styles/CartPage.css";
+import Navbar from "../components/Navbar";
+import { useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { useAPI } from "../contexts/APIContext";
+import CartItemCard from "../components/CartItemCard";
+import Cart from "../models/Cart";
+import CartItem from "../models/CartItem";
 
 const CartPage: React.FC = () => {
+    const { jwt } = useAuth();
+    const { cart, getCart } = useAPI();
+
+    useEffect(() => {
+        if (jwt !== null) {
+            getCart(jwt);
+        }
+    }, [jwt]);
+
     return (
-        <div>
+        <div className="page">
             <Navbar />
-            <h1>Cart Page</h1>
+            <div className="cart-page-content">
+                <h1>My Cart</h1>
+                <div className="cards-container">
+                    <div className="cart-item-cards">
+                        {cart?.cartItems.length !== 0 ? (
+                            cart?.cartItems.map((cartItem, index) => {
+                                return (
+                                    <CartItemCard
+                                        cartItemId={cartItem.cartItemId}
+                                        quantity={cartItem.quantity}
+                                        cartId={cartItem.cartId}
+                                        product={cartItem.product}
+                                    />
+                                );
+                            })
+                        ) : (
+                            <h2>Your cart is empty</h2>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default CartPage;
