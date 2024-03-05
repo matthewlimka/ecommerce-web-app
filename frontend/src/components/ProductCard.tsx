57b4a8c72b07e196c3faad82f73c8bcc46fd2147
Matthew Lim <matthewlimka@gmail.com>
2024-03-05 16:57:56 +0800

    Changed outer div to Link element, modified element classNames

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,18 +1,19 @@
 import "../styles/ProductCard.css";
+import { Link } from "react-router-dom";
 import Product from "../models/Product";
 
-const ProductCard = ({ productName, price, stockQuantity }: Product) => {
+const ProductCard = ({ productId, productName, price, stockQuantity }: Product) => {
     return (
-        <div className="productCard">
-            <div className="productImage">
+        <Link to={`/product/${productId}`} className="product-card">
+            <div className="product-card-image">
                 <img src="https://via.placeholder.com/150" alt="Product" />
             </div>
-            <h3>{productName}</h3>
-            <div className="productDetails">
-                <p>${price}</p>
-                <p>{stockQuantity} in stock</p>
+            <h3 className="product-card-product-name">{productName}</h3>
+            <div className="product-card-details">
+                <p className="product-card-details-price">${price}</p>
+                <p className="product-card-details-stock-quantity">{stockQuantity} in stock</p>
             </div>
-        </div>
+        </Link>
     );
 };
 
