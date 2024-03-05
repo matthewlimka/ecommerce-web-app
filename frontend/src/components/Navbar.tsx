8d0f7907b8eae771cfdca31c5a61ea1c74e49f8a
Matthew Lim <matthewlimka@gmail.com>
2024-03-05 16:56:19 +0800

    Restructured Navbar elements, updated paths for cart, profile and orders buttons, added navigation to HomePage upon logging out, modified useEffect and added cartRoute variable

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,76 +1,74 @@
-import '../styles/Navbar.css';
-import { useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
-import { useAPI } from '../contexts/APIContext';
-import Searchbar from './Searchbar';
+import "../styles/Navbar.css";
+import { useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { useAPI } from "../contexts/APIContext";
+import Searchbar from "./Searchbar";
 
 const Navbar: React.FC = () => {
-    
+
     const location = useLocation();
     const { jwt, logout } = useAuth();
     const { user, getUser, logoutUser } = useAPI();
+    const cartRoute: string = jwt !== null ? `/cart/${user?.userId}` : "/login";
+    const navigate = useNavigate();
 
     useEffect(() => {
-        if (jwt !== null && user === null) {
-            getUser(jwt)
+        if (jwt !== null) {
+            getUser(jwt);
         } else {
-            logoutUser()
+            logoutUser();
         }
-    }, [jwt])
+    }, [jwt]);
 
     const getActiveClass = (path: string) => {
-        return location.pathname === path ? 'active' : '';
+        return location.pathname === path ? "active" : "";
     };
 
     const handleLogout = () => {
         logout();
+        navigate('/home');
     };
 
     return (
-        <div className='navBar'>
-            <div className='leftContainer'>
-                <div className='homeContainer'>
-                    <Link to='/home' className={getActiveClass('/home')}>
-                        Home
-                    </Link>
+        <div className="navBar">
+            <div className="leftContainer">
+                <div className="homeContainer">
+                    <Link to="/home" className={getActiveClass("/home")}>Home</Link>
                 </div>
-                <div className='productContainer'>
-                    <Link
-                        to='/products'
-                        className={getActiveClass('/products')}
-                    >
-                        Products
-                    </Link>
-                </div>
-                <div className='orderContainer'>
-                    <Link to='/orders' className={getActiveClass('/orders')}>
-                        Orders
-                    </Link>
+                <div className="productContainer">
+                    <Link to="/products" className={getActiveClass("/products")}>Products</Link>
                 </div>
                 <Searchbar />
+                <div className="cartContainer">
+                    <Link to={cartRoute} className={getActiveClass(`/cart/${user?.userId}`)}>Cart</Link>
+                </div>
             </div>
-            <div className='rightContainer'>
+            <div className="rightContainer">
                 {jwt !== null ? (
-                    <div className='loggedInContainer'>
-                        <div className='cartButton'>
-                            <Link to='/cart'>Cart</Link>
-                        </div>
-                        <div className='userProfile'>{user?.username}</div>
-                        <div className='profileButton'>
-                            <Link to='/profile'>Profile</Link>
-                        </div>
-                        <div className='logoutButton'>
-                            <p onClick={handleLogout}>Logout</p>
+                    <div className="loggedInContainer">
+                        <div className="accountContainer">
+                            <p className="accountButton">Account</p>
+                            <div className="accountDropdown">
+                                <div className="profileButton">
+                                    <Link to={`/profile/${user?.userId}`}>Profile</Link>
+                                </div>
+                                <div className="ordersButton">
+                                    <Link to={`/orders/${user?.userId}`}>Orders</Link>
+                                </div>
+                                <div className="logoutButton">
+                                    <p onClick={handleLogout}>Logout</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 ) : (
-                    <div className='notLoggedInContainer'>
-                        <div className='signupButton'>
-                            <Link to='/signup'>Signup</Link>
+                    <div className="notLoggedInContainer">
+                        <div className="signupButton">
+                            <Link to="/signup">Signup</Link>
                         </div>
-                        <div className='loginButton'>
-                            <Link to='/login'>Login</Link>
+                        <div className="loginButton">
+                            <Link to="/login">Login</Link>
                         </div>
                     </div>
                 )}
