29d449fb262f4b2f22ea2373b26b1ba628980b0f
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-03-07 20:27:58 +0800

    Added findByTotalAmount method

--- backend/src/main/java/com/matthewlim/ecommercewebapp/repositories/CartRepository.java
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/repositories/CartRepository.java
@@ -1,5 +1,7 @@
 package com.matthewlim.ecommercewebapp.repositories;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +14,7 @@ import com.matthewlim.ecommercewebapp.models.User;
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
+	List<Cart> findByTotalAmount(BigDecimal totalAmount);
 	Optional<Cart> findByUser(User user);
 	Optional<Cart> findByCartItems(CartItem cartItem);
 }
