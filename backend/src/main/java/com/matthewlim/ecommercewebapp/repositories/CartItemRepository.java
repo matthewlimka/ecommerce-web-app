4dfc1a8e62698ef821103126e076e72dd07167c4
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-03-07 20:27:28 +0800

    Added findBySubtotal method and deleteByCart method with Transactional annotation

--- backend/src/main/java/com/matthewlim/ecommercewebapp/repositories/CartItemRepository.java
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/repositories/CartItemRepository.java
@@ -1,9 +1,11 @@
 package com.matthewlim.ecommercewebapp.repositories;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.matthewlim.ecommercewebapp.models.Cart;
 import com.matthewlim.ecommercewebapp.models.CartItem;
@@ -13,6 +15,9 @@ import com.matthewlim.ecommercewebapp.models.Product;
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
 	List<CartItem> findByQuantity(int quantity);
+	List<CartItem> findBySubtotal(BigDecimal subtotal);
 	List<CartItem> findByCart(Cart cart);
 	List<CartItem> findByProduct(Product product);
+	@Transactional
+	void deleteByCart(Cart cart);
 }
