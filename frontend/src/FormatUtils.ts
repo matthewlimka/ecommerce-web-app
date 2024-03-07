4356eb5ae5c76812456eb8b87020a036719dc652
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-03-07 20:00:11 +0800

    Added formatCart, formatCartItem, formatOrder and formatOrderItem functions

--- frontend/src/FormatUtils.ts
+++ frontend/src/FormatUtils.ts
@@ -1,10 +1,44 @@
 import Product from "./models/Product"
+import Cart from "./models/Cart"
+import CartItem from "./models/CartItem"
+import Order from "./models/Order"
+import OrderItem from "./models/OrderItem"
 
 const FormatUtils = {
     formatProduct(product: Product) {
         return {
             ...product,
             price: parseFloat(product.price.toFixed(2)),
+        }
+    },
+
+    formatCart(cart: Cart) {
+        return {
+            ...cart,
+            totalAmount: parseFloat(cart.totalAmount.toFixed(2)),
+            cartItems: cart.cartItems.map(this.formatCartItem),
+        }
+    },
+
+    formatCartItem(cartItem: CartItem) {
+        return {
+            ...cartItem,
+            subtotal: parseFloat(cartItem.subtotal.toFixed(2)),
+        }
+    },
+
+    formatOrder(order: Order) {
+        return {
+            ...order,
+            totalAmount: parseFloat(order.totalAmount.toFixed(2)),
+            orderItems: order.orderItems.map(this.formatOrderItem),
+        }
+    },
+    
+    formatOrderItem(orderItem: OrderItem) {
+        return {
+            ...orderItem,
+            subtotal: parseFloat(orderItem.subtotal.toFixed(2)),
         }
     }
 }
