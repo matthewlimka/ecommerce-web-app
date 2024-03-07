326c50fa8cd6e4b54a6a14b9c618e8c3774bf008
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-03-07 20:15:13 +0800

    Added summary section with checkout button and added link to redirect user to HomePage if cart is empty

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,6 +1,7 @@
 import "../styles/CartPage.css";
 import Navbar from "../components/Navbar";
 import { useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { useAPI } from "../contexts/APIContext";
 import CartItemCard from "../components/CartItemCard";
@@ -9,13 +10,20 @@ import CartItem from "../models/CartItem";
 
 const CartPage: React.FC = () => {
     const { jwt } = useAuth();
-    const { cart, getCart } = useAPI();
+    const { cart, getCart, createCheckoutOrder } = useAPI();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (jwt !== null) {
             getCart(jwt);
         }
     }, [jwt]);
+
+    const handleCheckout = (event: any) => {
+        event.preventDefault();
+        createCheckoutOrder(jwt!);
+        navigate('/checkout');
+    }
 
     return (
         <div className="page">
@@ -25,21 +33,32 @@ const CartPage: React.FC = () => {
                 <div className="cards-container">
                     <div className="cart-item-cards">
                         {cart?.cartItems.length !== 0 ? (
-                            cart?.cartItems.map((cartItem, index) => {
+                            cart?.cartItems.map((cartItem) => {
                                 return (
                                     <CartItemCard
                                         cartItemId={cartItem.cartItemId}
                                         quantity={cartItem.quantity}
+                                        subtotal={cartItem.subtotal}
                                         cartId={cartItem.cartId}
                                         product={cartItem.product}
                                     />
                                 );
                             })
                         ) : (
-                            <h2>Your cart is empty</h2>
+                            <div className="empty-cart-content">
+                                <h2>Your cart is empty</h2>
+                                <Link to="/home">Browse our products</Link>
+                            </div>
                         )}
                     </div>
                 </div>
+                {cart?.cartItems.length !== 0 && (
+                    <div className="cart-page-summary-section">
+                        <h2>Summary</h2>
+                        <h3>Total: ${cart?.totalAmount}</h3>
+                        <button onClick={handleCheckout} className="cart-page-action-button">Checkout</button>
+                    </div>
+                )}
             </div>
         </div>
     );
