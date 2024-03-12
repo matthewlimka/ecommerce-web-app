92700e3ed7ad2138eb1297b8175e03de38779ede
Matthew Lim <matthewlimka@gmail.com>
2024-03-12 17:38:32 +0800

    Renamed findByAddress method to findByShippingAddress

--- backend/src/main/java/com/matthewlim/ecommercewebapp/repositories/UserRepository.java
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/repositories/UserRepository.java
@@ -14,6 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	
 	Optional<User> findByUsername(String username);
 	Optional<User> findByEmail(String email);
-	Optional<User> findByAddress(Address address);
+	Optional<User> findByShippingAddress(Address shippingAddress);
 	Optional<User> findByOrders(Order order);
 }
