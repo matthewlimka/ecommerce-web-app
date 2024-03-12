e80a9016d2d85c618f42b9ded71967f6e38041d7
Matthew Lim <matthewlimka@gmail.com>
2024-03-12 17:22:04 +0800

    Re-organized page HTML, added shipping and payment method sections and changed redirect to CheckoutSuccessPage

--- frontend/src/pages/CheckoutPage.tsx
+++ frontend/src/pages/CheckoutPage.tsx
@@ -1,14 +1,18 @@
+import "../styles/CheckoutPage.css";
 import Navbar from "../components/Navbar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { useAPI } from "../contexts/APIContext";
 import OrderItemCard from "../components/OrderItemCard";
+import PaymentMethod from "../enums/PaymentMethod";
 
 const CheckoutPage: React.FC = () => {
 
     const { jwt } = useAuth();
-    const { checkoutOrder, placeOrder, clearCart } = useAPI();
+    const { user, checkoutOrder, placeOrder, clearCart } = useAPI();
+    const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<PaymentMethod>();
+    const shippingAddress: string = user?.shippingAddress?.streetAddress !== null ? `${user?.shippingAddress?.streetAddress}, Singapore ${user?.shippingAddress?.postalCode}` : 'No shipping address';
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -17,40 +21,60 @@ const CheckoutPage: React.FC = () => {
         }
     }, [jwt]);
 
-
     const handlePlaceOrder = (event: any) => {
         event.preventDefault();
-        placeOrder(jwt!);
+        placeOrder(jwt!, selectedPaymentMethod!);
         clearCart(jwt!);
-        navigate('/payment');
+        navigate('/checkout/success');
     }
 
     return (
         <div className="page">
             <Navbar />
             <div className="checkout-page-content">
-                <h1>Checkout</h1>
-                <div className="checkout-items-section">
-                    <h2>Products Ordered</h2>
-                    <div className="cards-container">
-                        <div className="checkout-item-cards">
-                            {checkoutOrder?.orderItems.map((orderItem) => {
-                                return (
-                                    <OrderItemCard
-                                        orderItemId={orderItem.orderItemId}
-                                        quantity={orderItem.quantity}
-                                        subtotal={orderItem.subtotal}
-                                        orderId={orderItem.orderId}
-                                        product={orderItem.product}
-                                    />
-                                );
-                            })}
-                        </div>
+                <h1 className="checkout-page-header">Checkout</h1>
+                <div className="checkout-page-shipping-section">
+                    <h2>Shipping Address</h2>
+                    <p>{shippingAddress.toUpperCase()}</p>
+                </div>
+                <div className="checkout-page-items-section">
+                    <h2 className="checkout-items-section-header">Products Ordered</h2>
+                    {checkoutOrder?.orderItems.map((orderItem) => (
+                        <OrderItemCard
+                            orderItemId={orderItem.orderItemId}
+                            quantity={orderItem.quantity}
+                            subtotal={orderItem.subtotal}
+                            orderId={orderItem.orderId}
+                            product={orderItem.product}
+                        />
+                    ))}
+                </div>
+                <div className="checkout-page-payment-section">
+                    <h2>Payment Method</h2>
+                    <div className="checkout-page-payment-options">
+                        <button
+                            className={`checkout-page-payment-option ${selectedPaymentMethod === PaymentMethod.PAYNOW ? 'selected' : ''}`}
+                            onClick={() => setSelectedPaymentMethod(PaymentMethod.PAYNOW)}
+                        >
+                            PayNow
+                        </button>
+                        <button
+                            className={`checkout-page-payment-option ${selectedPaymentMethod === PaymentMethod.CREDIT_CARD_MASTERCARD ? 'selected' : ''}`}
+                            onClick={() => setSelectedPaymentMethod(PaymentMethod.CREDIT_CARD_MASTERCARD)}
+                        >
+                            Mastercard
+                        </button>
+                        <button
+                            className={`checkout-page-payment-option ${selectedPaymentMethod === PaymentMethod.CREDIT_CARD_VISA ? 'selected' : ''}`}
+                            onClick={() => setSelectedPaymentMethod(PaymentMethod.CREDIT_CARD_VISA)}
+                        >
+                            Visa
+                        </button>
                     </div>
                 </div>
-                <div className="checkout-summary-section">
+                <div className="checkout-page-summary-section">
                     <h2>Summary</h2>
-                    <h3>Total: ${checkoutOrder?.totalAmount}</h3>
+                    <h3>Total ({checkoutOrder?.orderItems.length} item{checkoutOrder?.orderItems.length === 1 ? '' : 's'}): ${checkoutOrder?.totalAmount}</h3>
                     <button onClick={handlePlaceOrder} className="cart-page-action-button">Place Order</button>
                 </div>
             </div>
