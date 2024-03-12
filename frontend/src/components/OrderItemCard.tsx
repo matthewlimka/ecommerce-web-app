9eda2f11ae19f0521462c2d8641d1db429d6d238
Matthew Lim <matthewlimka@gmail.com>
2024-03-12 17:17:13 +0800

    Updated component HTML and added classNames for each HTML element

--- frontend/src/components/OrderItemCard.tsx
+++ frontend/src/components/OrderItemCard.tsx
@@ -1,3 +1,4 @@
+import "../styles/ItemCard.css";
 import OrderItem from "../models/OrderItem";
 
 const OrderItemCard = ({ orderItemId, quantity, subtotal, orderId, product }: OrderItem) => {
@@ -7,10 +8,19 @@ const OrderItemCard = ({ orderItemId, quantity, subtotal, orderId, product }: Or
             <div className="order-item-image">
                 <img src="https://via.placeholder.com/150" alt="OrderItem" />
             </div>
-            <h3>{product.productName}</h3>
-            <h4>Amount: {quantity}</h4>
-            <h4>Unit Price: ${product.price.toFixed(2)}</h4>
-            <h4>Item Subtotal: ${subtotal.toFixed(2)}</h4>
+            <h3 className="order-item-product-name">{product.productName}</h3>
+            <div className="order-item-right-section">
+                <div className="order-item-card-headers">
+                    <h4 className="order-item-price-header">Unit Price</h4>
+                    <h4 className="order-item-quantity-header">Amount</h4>
+                    <h4 className="order-item-subtotal-header">Item Subtotal</h4>
+                </div>
+                <div className="order-item-card-values">
+                    <h4 className="order-item-price">${product.price.toFixed(2)}</h4>
+                    <h4 className="order-item-quantity">{quantity}</h4>
+                    <h4 className="order-item-subtotal">${subtotal.toFixed(2)}</h4>
+                </div>
+            </div>
         </div>
     );
 }
