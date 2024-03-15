614b17de662b607053abe2f1256ff5809ae6684a
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-03-15 17:02:17 +0800

    Added routing for RegistrationPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { AuthProvider } from './contexts/AuthContext';
 import { APIProvider } from './contexts/APIContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
 import OAuthCallbackPage from './pages/OAuthCallbackPage';
 import ProductPage from './pages/ProductPage';
 import OrderHistoryPage from './pages/OrderHistoryPage';
@@ -23,6 +24,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/home" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<RegistrationPage />} />
             <Route path="/oauth/callback" element={<OAuthCallbackPage />} />
             <Route path="/product/:productId" element={<ProductPage />} />
             <Route path="/orders/:userId" element={<OrderHistoryPage />} />
