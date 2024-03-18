e75b4c09e16bd7d781090f9aebb6ed8f7c93ad2a
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-03-18 22:59:37 +0800

    Updated filter to handle form login requests with invalid username or password

--- backend/src/main/java/com/matthewlim/ecommercewebapp/auth/JwtAuthorizationFilter.java
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/auth/JwtAuthorizationFilter.java
@@ -8,12 +8,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.matthewlim.ecommercewebapp.exceptions.UserNotFoundException;
+import com.matthewlim.ecommercewebapp.models.User;
+import com.matthewlim.ecommercewebapp.services.UserService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -26,6 +30,8 @@ import lombok.AllArgsConstructor;
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtDecoder jwtDecoder;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -47,7 +53,57 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid or expired JWT");
                 return;
             }
+        } else if (isLoginRequest(request)) {
+        	if (isAuthenticationRequestValid(request, response)) {
+        		filterChain.doFilter(request, response);
+        	}
+        	return;
         }
+        
         filterChain.doFilter(request, response);
+    }
+    
+    private boolean isLoginRequest(HttpServletRequest request) {
+    	return "/login".equals(request.getRequestURI()) && "POST".equalsIgnoreCase(request.getMethod());
+    }
+    
+    private boolean isAuthenticationRequestValid(HttpServletRequest request, HttpServletResponse response) {
+    	String username = request.getParameter("username");
+    	String password = request.getParameter("password");
+    	return isValid(username, password, response);
+    }
+    
+    private boolean isValid(String username, String password, HttpServletResponse response) {
+    	try {
+    		User user = userService.findByUsername(username);
+    		
+    		if (user != null) {
+    			String validPassword = user.getPassword();
+    			
+    			if (passwordEncoder.matches(password, validPassword)) {
+    				return true;
+    			} else {
+    				logger.info("Invalid password for user: " + username);
+    				try {
+    					response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid password");    					
+    				} catch (IOException ioException) {
+    		    		logger.error(ioException);
+    		    	}
+    				return false;
+    			}
+    		} else {
+
+    		}
+    	} catch (UserNotFoundException userNotFoundException) {
+    		logger.error(userNotFoundException);
+    		logger.info("Invalid username: " + username);
+    		try {
+    			response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid username");    			
+    		} catch (IOException ioException) {
+    			logger.error(ioException);
+    		}
+			return false;
+    	} 
+    	return false;
     }
 }
