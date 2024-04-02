3ce92dcf60c304f995e46c88ecbc226f45f9cf1b
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-04-02 16:18:52 +0800

    Re-organized headers and values displayed in the component

--- frontend/src/components/OrderCard.tsx
+++ frontend/src/components/OrderCard.tsx
@@ -1,7 +1,6 @@
 import "../styles/OrderCard.css";
 import { useState } from "react";
 import Order from "../models/Order";
-import OrderStatus from "../enums/OrderStatus";
 import FormatUtils from "../FormatUtils";
 
 const OrderCard: React.FC<{ order: Order }> = ({ order }) => {
@@ -14,43 +13,21 @@ const OrderCard: React.FC<{ order: Order }> = ({ order }) => {
 
     return (
         <div className={`order-card ${expanded ? 'expanded' : 'collapsed'}`} onClick={handleToggleExpand}>
-            <div className="order-card-top-section">
-                <div className="order-card-order-details">
-                    <div className="order-card-detail-line">
-                        <h3 className="order-card-detail-label">Order ID:</h3>
-                        <span>{order?.orderId}</span>
-                    </div>
-                    <div className="order-card-detail-line">
-                        <h3 className="order-card-detail-label">Order Date:</h3>
-                        <span>{order?.orderDate.toDateString()}</span>
-                    </div>
-                    <div className="order-card-detail-line">
-                        <h3 className="order-card-detail-label">Total:</h3>
-                        <span>${order?.totalAmount.toFixed(2)}</span>
-                    </div>
+            <div className="order-card-order-details">
+                <div className="order-card-order-details-header-section">
+                    <h3 id="order" className="order-card-order-details-header">Order</h3>
+                    <h3 id="order-date" className="order-card-order-details-header">Date Ordered</h3>
+                    <h3 id="payment-method" className="order-card-order-details-header">Payment Method</h3>
+                    <h3 id="delivery-status" className="order-card-order-details-header">Delivery Status</h3>
+                    <h3 id="total" className="order-card-order-details-header">Total</h3>
                 </div>
-                <div className="order-card-payment-details">
-                    <div className="order-card-detail-line">
-                        <h3 className="order-card-detail-label">Payment Method:</h3>
-                        <span>{FormatUtils.formatPaymentMethod(order?.payment?.paymentMethod!)}</span>
-                    </div>
-                    <div className="order-card-detail-line">
-                        <h3 className="order-card-detail-label">Payment ID:</h3>
-                        <span>{order?.payment?.paymentId}</span>
-                    </div>
-                    <div className="order-card-detail-line">
-                        <h3 className="order-card-detail-label">Transaction ID:</h3>
-                        <span>{order?.payment?.transactionId}</span>
-                    </div>
+                <div className="order-card-order-details-value-section">
+                    <span id="order" className="order-card-order-details-value">{order?.orderId}</span>
+                    <span id="order-date" className="order-card-order-details-value">{order?.orderDate.toDateString()}</span>
+                    <span id="payment-method" className="order-card-order-details-value">{FormatUtils.formatPaymentMethod(order?.payment?.paymentMethod!)}</span>
+                    <span id="delivery-status" className="order-card-order-details-value">{order?.orderStatus}</span>                        
+                    <span id="total" className="order-card-order-details-value">${order?.totalAmount.toFixed(2)}</span>
                 </div>
-            </div>
-            <div className="order-card-status-section">
-                {Object.values(OrderStatus).map(status => (
-                    <div key={status} className={`order-card-status ${status === order.orderStatus ? 'current' : ''}`}>
-                        <div className="order-card-status-circle"></div>
-                        <span>{status}</span>
-                    </div>
-                ))}
             </div>
             {expanded && (
                 <div className="order-card-order-items">
