190c676efb05cb9f38d55ef427a643f41495879c
Matthew Lim <matthewlimka@gmail.com>
2024-04-04 16:04:02 +0800

    Added import for AppConfig file

--- backend/src/main/java/com/matthewlim/ecommercewebapp/ECommerceWebAppApplication.java
+++ backend/src/main/java/com/matthewlim/ecommercewebapp/ECommerceWebAppApplication.java
@@ -3,15 +3,17 @@ package com.matthewlim.ecommercewebapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
 
+import com.matthewlim.ecommercewebapp.auth.AppConfig;
 import com.matthewlim.ecommercewebapp.auth.RsaKeyProperties;
 
-@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
+@Import(AppConfig.class)
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class ECommerceWebAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceWebAppApplication.class, args);
 	}
-
 }
