0c4bae24bb14c03acc8f4e2eb7f9653cf8e6f75f
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-04-08 16:40:43 +0800

    Fixed testPartialUpdateCartItem method and added mock cart repository

--- backend/src/test/java/com/matthewlim/ecommercewebapp/unit/services/CartItemServiceUnitTest.java
+++ backend/src/test/java/com/matthewlim/ecommercewebapp/unit/services/CartItemServiceUnitTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -24,6 +25,7 @@ import com.matthewlim.ecommercewebapp.models.Cart;
 import com.matthewlim.ecommercewebapp.models.CartItem;
 import com.matthewlim.ecommercewebapp.models.Product;
 import com.matthewlim.ecommercewebapp.repositories.CartItemRepository;
+import com.matthewlim.ecommercewebapp.repositories.CartRepository;
 import com.matthewlim.ecommercewebapp.services.CartItemService;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,6 +33,9 @@ public class CartItemServiceUnitTest {
 
 	@Mock
 	private CartItemRepository cartItemRepository;
+	
+	@Mock
+	private CartRepository cartRepository;
 	
 	@InjectMocks
 	private CartItemService cartItemService;
@@ -134,12 +139,14 @@ public class CartItemServiceUnitTest {
 		Long cartItemId = 1L;
 		CartItem existingCartItem = new CartItem();
 		existingCartItem.setCartItemId(cartItemId);
+		Cart cart = new Cart();
 		
 		Map<String, Object> fieldsToUpdate = new HashMap<String, Object>();
 		fieldsToUpdate.put("quantity", 10);
-		fieldsToUpdate.put("product", new Product());
+		fieldsToUpdate.put("product", new Product("Test product", BigDecimal.valueOf(32.05), 23));
 		
 		when(cartItemRepository.findById(cartItemId)).thenReturn(Optional.of(existingCartItem));
+		when(cartRepository.findByCartItems(existingCartItem)).thenReturn(Optional.of(cart));
 		when(cartItemRepository.save(existingCartItem)).thenReturn(existingCartItem);
 		CartItem result = cartItemService.partialUpdateCartItem(cartItemId, fieldsToUpdate);
 		
