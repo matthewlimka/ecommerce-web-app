2c269876c4dee669ada943c999cb81b2341d5efe
Matthew Lim <matthewlimka@gmail.com>
2024-04-09 14:27:32 +0800

    Added TestPropertySource annotation to use application-test.properties in test resources instead of application.properties in main resources

--- backend/src/test/java/com/matthewlim/ecommercewebapp/integration/controllers/CartItemControllerIntegrationTest.java
+++ backend/src/test/java/com/matthewlim/ecommercewebapp/integration/controllers/CartItemControllerIntegrationTest.java
@@ -24,6 +24,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -38,6 +39,7 @@ import com.matthewlim.ecommercewebapp.repositories.ProductRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class CartItemControllerIntegrationTest {
 
 	@Autowired
